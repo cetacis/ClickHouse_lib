5fe003469bce095911e76d1c3d67952763b72a8f
Amos Bird <amosbird@gmail.com>
2019-10-08 17:55:44 +0800

    update

--- client.sh
+++ client.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-/home/amos/gentoo/lib64/ld-linux-x86-64.so.2 build/dbms/programs/clickhouse client
+/home/amos/gentoo/lib64/ld-linux-x86-64.so.2 build/dbms/programs/clickhouse client "$@"
