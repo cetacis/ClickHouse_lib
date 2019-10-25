3777e4ba938ad5b47f830339bb24ed39be1a2444
Amos Bird <amosbird@gmail.com>
2019-10-26 02:38:33 +0800

    even more

--- client.sh
+++ client.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-"$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"/build/dbms/programs/clickhouse client "$@"
+case "$(basename "$0")" in
+    cq)
+        "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"/build/dbms/programs/clickhouse client -mn <<< "$*"
+        ;;
+    c)
+        "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"/build/dbms/programs/clickhouse client -n "$@"
+        ;;
+esac
