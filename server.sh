eab1dafa321994465ac264e63173111a757223ed
Amos Bird <amosbird@gmail.com>
2019-11-05 00:37:03 +0800

    fix

--- server.sh
+++ server.sh
@@ -3,16 +3,17 @@
 export LD_BIND_NOW=1
 
 clickhouse="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"/build/dbms/programs/clickhouse
+config_path="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"/etc
 
 case "$(basename "$0")" in
 s)
-    $clickhouse server --config etc/config.xml "$@"
+    $clickhouse server --config "$config_path"/config.xml "$@"
     ;;
 s5)
-    $clickhouse server --config etc/config-s5.xml "$@"
+    $clickhouse server --config "$config_path"/config-s5.xml "$@"
     ;;
 so)
-    "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"/build-origin/dbms/programs/clickhouse server --config etc/config.xml "$@"
+    "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"/build-origin/dbms/programs/clickhouse server --config "$config_path"/config.xml "$@"
     ;;
 *)
     echo "There is no server called $0 yet."
