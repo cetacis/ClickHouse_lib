ClickHouse/ClickHouse

Support empty tuple.

src/DataTypes/Serializations/SerializationTuple.cpp
@@ -97,6 +97,8 @@ static void addElementSafe(size_t num_elems, IColumn & column, F && impl)
 
         throw;
     }
+
+    typeid_cast<ColumnTuple &>(column).addSize(1);
 }
 
 void SerializationTuple::deserializeBinary(IColumn & column, ReadBuffer & istr, const FormatSettings & settings) const
@@ -398,6 +400,12 @@ void SerializationTuple::enumerateStreams(
     const StreamCallback & callback,
     const SubstreamData & data) const
 {
+    if (elems.empty())
+    {
+        ISerialization::enumerateStreams(settings, callback, data);
+        return;
+    }
+
     const auto * type_tuple = data.type ? &assert_cast<const DataTypeTuple &>(*data.type) : nullptr;
     const auto * column_tuple = data.column ? &assert_cast<const ColumnTuple &>(*data.column) : nullptr;
     const auto * info_tuple = data.serialization_info ? &assert_cast<const SerializationInfoTuple &>(*data.serialization_info) : nullptr;
@@ -468,6 +476,22 @@ void SerializationTuple::serializeBinaryBulkWithMultipleStreams(
     SerializeBinaryBulkSettings & settings,
     SerializeBinaryBulkStatePtr & state) const
 {
+    if (elems.empty())
+    {
+        if (WriteBuffer * stream = settings.getter(settings.path))
+        {
+            size_t size = column.size();
+
+            if (limit == 0 || offset + limit > size)
+                limit = size - offset;
+
+            for (size_t i = 0; i < limit; ++i)
+                stream->write('0');
+        }
+
+        return;
+    }
+
     auto * tuple_state = checkAndGetState<SerializeBinaryBulkStateTuple>(state);
 
     for (size_t i = 0; i < elems.size(); ++i)
@@ -484,6 +508,24 @@ void SerializationTuple::deserializeBinaryBulkWithMultipleStreams(
     DeserializeBinaryBulkStatePtr & state,
     SubstreamsCache * cache) const
 {
+    if (elems.empty())
+    {
+        auto cached_column = getFromSubstreamsCache(cache, settings.path);
+        if (cached_column)
+        {
+            column = cached_column;
+        }
+        else if (ReadBuffer * stream = settings.getter(settings.path))
+        {
+            auto mutable_column = column->assumeMutable();
+            typeid_cast<ColumnTuple &>(*mutable_column).addSize(stream->tryIgnore(limit));
+            column = std::move(mutable_column);
+            addToSubstreamsCache(cache, settings.path, column);
+        }
+
+        return;
+    }
+
     auto * tuple_state = checkAndGetState<DeserializeBinaryBulkStateTuple>(state);
 
     auto mutable_column = column->assumeMutable();
@@ -492,6 +534,8 @@ void SerializationTuple::deserializeBinaryBulkWithMultipleStreams(
     settings.avg_value_size_hint = 0;
     for (size_t i = 0; i < elems.size(); ++i)
         elems[i]->deserializeBinaryBulkWithMultipleStreams(column_tuple.getColumnPtr(i), limit, settings, tuple_state->states[i], cache);
+
+    typeid_cast<ColumnTuple &>(*mutable_column).addSize(column_tuple.getColumn(0).size());
 }
 
 size_t SerializationTuple::getPositionByName(const String & name) const

cetacis/ClickHouse_lib

src/DataTypes/Serializations/SerializationTuple.cpp
@@ -91,6 +91,10 @@ static ReturnType addElementSafe(size_t num_elems, IColumn & column, F && impl)
             restore_elements();
             return ReturnType(false);
         }
+        else
+        {
+            assert_cast<ColumnTuple &>(column).addSize(1);
+        }
 
         // Check that all columns now have the same size.
         size_t new_size = column.size();
@@ -554,6 +558,12 @@ void SerializationTuple::enumerateStreams(
     const StreamCallback & callback,
     const SubstreamData & data) const
 {
+    if (elems.empty())
+    {
+        ISerialization::enumerateStreams(settings, callback, data);
+        return;
+    }
+
     const auto * type_tuple = data.type ? &assert_cast<const DataTypeTuple &>(*data.type) : nullptr;
     const auto * column_tuple = data.column ? &assert_cast<const ColumnTuple &>(*data.column) : nullptr;
     const auto * info_tuple = data.serialization_info ? &assert_cast<const SerializationInfoTuple &>(*data.serialization_info) : nullptr;
@@ -624,6 +634,22 @@ void SerializationTuple::serializeBinaryBulkWithMultipleStreams(
     SerializeBinaryBulkSettings & settings,
     SerializeBinaryBulkStatePtr & state) const
 {
+    if (elems.empty())
+    {
+        if (WriteBuffer * stream = settings.getter(settings.path))
+        {
+            size_t size = column.size();
+
+            if (limit == 0 || offset + limit > size)
+                limit = size - offset;
+
+            for (size_t i = 0; i < limit; ++i)
+                stream->write('0');
+        }
+
+        return;
+    }
+
     auto * tuple_state = checkAndGetState<SerializeBinaryBulkStateTuple>(state);
 
     for (size_t i = 0; i < elems.size(); ++i)
@@ -640,6 +666,24 @@ void SerializationTuple::deserializeBinaryBulkWithMultipleStreams(
     DeserializeBinaryBulkStatePtr & state,
     SubstreamsCache * cache) const
 {
+    if (elems.empty())
+    {
+        auto cached_column = getFromSubstreamsCache(cache, settings.path);
+        if (cached_column)
+        {
+            column = cached_column;
+        }
+        else if (ReadBuffer * stream = settings.getter(settings.path))
+        {
+            auto mutable_column = column->assumeMutable();
+            typeid_cast<ColumnTuple &>(*mutable_column).addSize(stream->tryIgnore(limit));
+            column = std::move(mutable_column);
+            addToSubstreamsCache(cache, settings.path, column);
+        }
+
+        return;
+    }
+
     auto * tuple_state = checkAndGetState<DeserializeBinaryBulkStateTuple>(state);
 
     auto mutable_column = column->assumeMutable();
@@ -648,6 +692,8 @@ void SerializationTuple::deserializeBinaryBulkWithMultipleStreams(
     settings.avg_value_size_hint = 0;
     for (size_t i = 0; i < elems.size(); ++i)
         elems[i]->deserializeBinaryBulkWithMultipleStreams(column_tuple.getColumnPtr(i), limit, settings, tuple_state->states[i], cache);
+
+    typeid_cast<ColumnTuple &>(*mutable_column).addSize(column_tuple.getColumn(0).size());
 }
 
 size_t SerializationTuple::getPositionByName(const String & name) const